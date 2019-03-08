94ca5505ea2969bfe9f22e6d601cc5bbde9c9c2c
massakomi <andymc@inbox.ru>
2019-03-08 19:13:27 +0300

    Update avito.php
    Продолжаю писать скрипт парсинга авито на php. В этом видео сделал форму для управления парсингом, с полями урл и макс. страница. Также продолжил извлекать параметры объявления.

--- avito.php
+++ avito.php
@@ -1,5 +1,8 @@
 <?php
-//require_once 'init.php';
+
+
+
+// Технические функции
 
 function setCache($content, $cacheId)
 {
@@ -7,12 +10,6 @@ function setCache($content, $cacheId)
         return ;
     }
     $fileName = 'cash/'.md5($cacheId);
-    /*if (strpos($str, 'Мы обнаружили, что запросы')) {
-        if (file_exists($fileName)) {
-        	unlink($fileName);
-            return ;
-        }
-    }*/
     if (!file_exists('cash')) {
         mkdir('cash');
     }
@@ -34,11 +31,7 @@ function getCache($cacheId, $cashExpired=true, &$fileName='')
     if ($time > $cashExpired) {
         return false;
     }
-    /*if (strpos($str, 'Мы обнаружили, что запросы')) {
-        return ;
-    }*/
     return file_get_contents($fileName);
-
 }
 
 function curlLoad($url, $cash=0)
@@ -60,34 +53,6 @@ function curlLoad($url, $cash=0)
     return $content;
 }
 
-function parseAvitoPage($url)
-{
-    $content = curlLoad($url, $cash=3600);
-
-    preg_matchx('~<div class="js-catalog_before-ads">.*?<div class="avito-ads-container avito-ads-container_desktop_low">~is', $content, $a);
-    $innerContent = $a[0];
-
-    $rows = preg_split('~<div class="item item_table~is', $innerContent);
-    array_shift($rows);
-
-    //preg_match_all('~<div class="item item_table.*?</div>\s*</div>\s*</div>~is', $innerContent, $rows);
-
-    $data = [];
-    foreach ($rows as $key => $rowContent) {
-        //echo '<pre>'.htmlspecialchars($rowContent).'</pre>';
-
-        preg_match('~<span itemprop="name">(.*?)</span>~i', $rowContent, $a);
-        $name = $a[1];
-        preg_match('~itemprop="price"\s*content="(.*?)"~i', $rowContent, $a);
-        $price = $a[1];
-
-        //echo '<br />-'.($key + 1).') '.$name.' --- <b>'.$price.'</b>';
-        //exit;
-        $data []= compact('name', 'price');
-    }
-    return $data;
-}
-
 function preg_matchx($regexp, $content, &$results)
 {
     $res = preg_match($regexp, $content, $results);
@@ -106,7 +71,89 @@ function preg_match_allx($regexp, $content, &$results)
     return $res;
 }
 
+function POST($key, $default='')
+{
+    if (array_key_exists($key, $_POST)) {
+        return $_POST[$key];
+    } else {
+        return $default;
+    }
+}
 
+function GET($key, $default='')
+{
+    if (array_key_exists($key, $_GET)) {
+        return $_POST[$key];
+    } else {
+        return $default;
+    }
+}
+
+// Авито функции
+
+function getAvitoDate($dateString)
+{
+    $date = $dateString;
+
+    $date = str_replace('&nbsp;', ' ', $date);
+    $date = str_replace('Сегодня', date('Y-m-d'), $date);
+    $date = str_replace('Вчера', date('Y-m-d', strtotime(date('Y-m-d').' -1 day')), $date);
+    $date = trim($date);
+    $date .= ':00';
+
+    $date = strtotime($date);
+    return $date;
+}
+
+function parseAvitoPage($url)
+{
+    $content = curlLoad($url, $cash=3600);
+
+    preg_matchx('~<div class="js-catalog_before-ads">.*?<div class="avito-ads-container avito-ads-container_desktop_low">~is', $content, $a);
+    $innerContent = $a[0];
+
+    $rows = preg_split('~<div class="item item_table~is', $innerContent);
+    array_shift($rows);
+
+    //preg_match_all('~<div class="item item_table.*?</div>\s*</div>\s*</div>~is', $innerContent, $rows);
+
+    $data = [];
+    foreach ($rows as $key => $rowContent) {
+        //echo '<pre>'.htmlspecialchars($rowContent).'</pre>';
+
+        $row = [];
+
+        preg_match('~<span itemprop="name">(.*?)</span>~i', $rowContent, $a);
+        $row['name'] = $a[1];
+
+        preg_match('~\d{4}~i', $row['name'], $a);
+        $row['year'] = $a[0];
+
+        preg_match('~data-absolute-date="\s*([^"]+)\s*"~i', $rowContent, $a);
+        $row['date'] = getAvitoDate($a[1]);
+
+        preg_match('~data-item-url="([^"]+)"~i', $rowContent, $a);
+        $row['url'] = $a[1];
+
+        preg_match('~data-item-id="(\d+)"~i', $rowContent, $a);
+        $row['id'] = $a[1];
+
+        preg_match('~>\s+([\d\s]+) км~i', $rowContent, $a);
+        $row['probeg'] = preg_replace('~[^\d]~i', '', $a[1]);
+
+        preg_match('~</p>\s+<p>([^<]+)</p>~i', $rowContent, $a);
+        $row['region'] = $a[1];
+
+        preg_match('~itemprop="price"\s*content="(\d+)"~i', $rowContent, $a);
+        $row['price'] = $a[1];
+
+        //echo '<pre>'; print_r($row); echo '</pre>';
+
+        //exit;
+        $data []= $row;
+    }
+    return $data;
+}
 
 function parseAvitoAll($url, $fromPage=1, $maxPage=false)
 {
@@ -144,14 +191,75 @@ function parseAvitoAll($url, $fromPage=1, $maxPage=false)
 }
 
 
+
 $url = 'https://www.avito.ru/syktyvkar/avtomobili?radius=200';
-$data = parseAvitoAll($url, $fromPage=1, $maxPage=5);
+$url = $_POST['url'] ?: $url;
 
-var_dump(count($data));
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Парсер Авито</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 
-echo '<hr />';
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <style type="text/css">
+    h1 {margin:20px 0 15px; font-size:24px;}
+    </style>
+  </head>
+  <body>
+
+<div class="container-fluid">
+
+    <h1>Парсер Авито</h1>
+
+    <form class="form-inline" method="post">
+      <div class="form-group">
+        <label><a href="<?=$url?>" target="_blank">URL</a></label>
+        <input type="text" class="form-control" name="url" value="<?=$url?>" style="width:400px;">
+      </div>
+&nbsp;
+      <div class="form-group">
+        <label>Загружать до</label>
+        <input type="number" class="form-control" name="maxPage" value="<?=POST('maxPage', 1)?>" style="width:70px;">
+      </div>
+      <button type="submit" class="btn btn-default">Выполнить</button>
+    </form>
 
 
 
+<?php
+
+if ($_POST['url']) {
+    
+    $data = parseAvitoAll($_POST['url'], $fromPage=1, $_POST['maxPage']);
+
+    echo '<hr />';
+    echo '<pre>'; print_r($data); echo '</pre>';
+}
+
+
+
+
+?>
+
+</div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </body>
+</html>
 
